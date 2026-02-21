c33bd9d5c92bbc40a3e3deface7f234fe96c1c33
kasramo1383 <montazerikasra@gmail.com>
2026-02-21 16:03:53 +0330

    delete incorrect discounts

--- Part 1 - Data Extraction/2. Remove or change unacceptable data/incorrect discount.sql
+++ Part 1 - Data Extraction/2. Remove or change unacceptable data/incorrect discount.sql
@@ -1,4 +1,3 @@
--- Count rows with invalid discount values
-SELECT COUNT(*) AS invalid_discount_count
-FROM BDB
+-- Delete rows with invalid discount values
+DELETE FROM BDB
 WHERE Discount < 0 OR Discount > 1;
