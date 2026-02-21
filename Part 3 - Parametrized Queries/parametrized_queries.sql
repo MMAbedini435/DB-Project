e49de356939eedcf4c8e283077744b0f5c02fc3f
kasramo1383 <montazerikasra@gmail.com>
2026-02-21 07:26:30 +0330

    change based on domain

--- Part 3 - Parametrized Queries/parametrized_queries.sql
+++ Part 3 - Parametrized Queries/parametrized_queries.sql
@@ -75,8 +75,8 @@ HAVING COUNT(DISTINCT op.ord_id) >= $2;
 --------- DELAYED PRODUCTS
 SELECT o.ord_id
 FROM orders o
-WHERE (o.send_type = 'Same day delivery' AND DATE(o.send_time) <> DATE(o.reg_time))
-   OR (o.send_type = 'Normal' AND o.send_time > o.reg_time + INTERVAL '2 days');
+WHERE (o.send_type = 'Same-Day' AND DATE(o.send_time) <> DATE(o.reg_time))
+   OR (o.send_type = 'Ordinary' AND o.send_time > o.reg_time + INTERVAL '2 days');
 
 
 
